4b94fdd14ee09a25c613c3f2b7c78e49b1ba5f87
Bruno Gonzales <57231813+brunogonzales@users.noreply.github.com>
2022-02-23 11:16:13 -0500

    Initial commit

---
+++ components/perfil.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>Mi nombre</h3>
+    <pre><code>{{user}}</code></pre>
+    <button v-if="!user.addr" @click="$fcl.authenticate">login</button>
+    <button v-else @click="$fcl.unauthenticate">logout</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      user: {},
+    };
+  },
+  mounted() {
+    this.$fcl.currentUser().subscribe((user) => (this.user = user));
+  },
+};
+</script>
---
+++ plugins/fcl.client.js
@@ -0,0 +1,10 @@
+import * as fcl from "@onflow/fcl";
+
+fcl.config({
+  "accessNode.api": "https://access-testnet.onflow.org",
+  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn"
+});
+
+export default ({ app }, inject) => {
+  inject("fcl", fcl);
+};
